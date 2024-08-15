4375fc9a36b69170b990f612f951213cfdb1e5b2
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2024-08-15 07:54:19 +0200

    Switch to Dune

---
+++ src/oByteLib.ml
@@ -0,0 +1,32 @@
+(*************************************************************************)
+(*                                                                       *)
+(*                              OByteLib                                 *)
+(*                                                                       *)
+(*                            Benoit Vaugon                              *)
+(*                                                                       *)
+(*    This file is distributed under the terms of the CeCILL license.    *)
+(*    See file ../LICENSE-en.                                            *)
+(*                                                                       *)
+(*************************************************************************)
+
+module Version = Version
+module Section = Section
+module Index = Index
+module Extra = Extra
+module Dlpt = Dlpt
+module Dlls = Dlls
+module Crcs = Crcs
+module Ident = Ident
+module Symb = Symb
+module Dbug = Dbug
+module Value = Value
+module Data = Data
+module Prim = Prim
+module Instr = Instr
+module Code = Code
+module Bytefile = Bytefile
+module Cmofile = Cmofile
+module Normalised_instr = Normalised_instr
+module Normalised_code = Normalised_code
+module Interp = Interp
+module Normalised_interp = Normalised_interp
